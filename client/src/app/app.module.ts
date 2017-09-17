4a9cfe23f7c478457e42e3227eb13a5852163fd8
Delphin Germa <mirrar@free.fr>
2017-09-17 12:42:42 +0200

    Initialize routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,44 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {ModalModule} from 'ngx-bootstrap/modal';
-import {CookieService} from 'angular2-cookie/services/cookies.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {AllshopsComponent} from './allshops/allshops.component';
-import {YoursshopsComponent} from './yoursshops/yoursshops.component';
-import {InventoryComponent} from './inventory/inventory.component';
-import {IdentificationComponent} from './identification/identification.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AllshopsComponent } from './allshops/allshops.component';
+import { YoursshopsComponent } from './yoursshops/yoursshops.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { IdentificationComponent } from './identification/identification.component';
 
-import {LogService} from './services/log/log.service';
-import {LoadingService} from './services/loading/loading.service';
-import {CommunicationService} from './services/communication/communication.service';
-import {PositionService} from './services/position/position.service';
-import {PlayersService} from './services/players/players.service';
-import {ShopsService} from './services/shops/shops.service';
+import { LogService } from './services/log/log.service';
+import { LoadingService } from './services/loading/loading.service';
+import { CommunicationService } from './services/communication/communication.service';
+import { PositionService } from './services/position/position.service';
+import { PlayersService } from './services/players/players.service';
+import { ShopsService } from './services/shops/shops.service';
 import { StatusbarComponent } from './statusbar/statusbar.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'allshops',
+        component: AllshopsComponent
+    },
+    {
+        path: 'yoursshops',
+        component: YoursshopsComponent
+    },
+    {
+        path: 'inventory',
+        component: InventoryComponent
+    }
+];
 
 @NgModule({
     declarations: [
@@ -34,9 +54,10 @@ import { StatusbarComponent } from './statusbar/statusbar.component';
         BrowserModule,
         FormsModule,
         HttpClientModule,
-        ModalModule.forRoot()
+        ModalModule.forRoot(),
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [LogService, LoadingService, CookieService, CommunicationService, PositionService, PlayersService, ShopsService],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
